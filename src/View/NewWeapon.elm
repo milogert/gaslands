9774d9f20550c3f2399020cfc1211a7e5457148b
Milo Gertjejansen <milo.gertjejansen@mediabeacon.com>
2018-05-18 13:02:07 -0500

    Updated new weapon page style and layout

--- src/View/NewWeapon.elm
+++ src/View/NewWeapon.elm
@@ -1,7 +1,7 @@
 module View.NewWeapon exposing (view)
 
 import Html exposing (Html, button, div, h1, h2, h3, h4, h5, h6, img, input, label, li, node, option, p, select, small, span, text, textarea, ul)
-import Html.Attributes exposing (checked, class, disabled, for, href, id, max, min, placeholder, rel, src, type_, value)
+import Html.Attributes exposing (checked, class, disabled, for, href, id, max, min, placeholder, rel, src, type_, value, multiple, size)
 import Html.Events exposing (onClick, onInput)
 import Model.Model exposing (..)
 import Model.Vehicles exposing (..)
@@ -28,22 +28,26 @@ view model v =
         isCrewFree =
             crewLeft > 0
     in
-    div []
-        [ select
-            [ onInput TmpWeaponUpdate
-            , class "form-control"
+    View.Utils.row
+        [ View.Utils.col "md-3"
+            [ button [ class "form-control btn btn-primary mb-2", onClick (AddWeapon v) ] [ text "Add Weapon" ]
+            , select
+                [ onInput TmpWeaponUpdate
+                , class "form-control"
+                , multiple True
+                , size 8
+                ]
+                (option [] []
+                    :: (List.filter (\x -> x.slots <= slotsLeft) allWeaponsList
+                            |> List.filter
+                                (\x ->
+                                    (not <| List.member CrewFired x.specials)
+                                        || (isCrewFree && List.member CrewFired x.specials)
+                                )
+                            |> List.map .name
+                            |> List.map (\t -> option [ value t ] [ text t ])
+                       )
+                )
             ]
-            (option [] []
-                :: (List.filter (\x -> x.slots <= slotsLeft) allWeaponsList
-                        |> List.filter
-                            (\x ->
-                                (not <| List.member CrewFired x.specials)
-                                    || (isCrewFree && List.member CrewFired x.specials)
-                            )
-                        |> List.map .name
-                        |> List.map (\t -> option [ value t ] [ text t ])
-                   )
-            )
-        , View.Weapon.render v model.tmpWeapon
-        , button [ class "form-control btn btn-primary", onClick (AddWeapon v) ] [ text "Add Weapon" ]
+        , View.Utils.col "md-9" [ View.Weapon.render v model.tmpWeapon ]
         ]
