ead4cb8a1910f3d1d9b9d4a8710846bd99f8168c
Milo Gertjejansen <milo@milogert.com>
2019-03-29 14:31:25 -0500

    Fixed bug for sponsor update
    continues #23

--- src/Update/Sponsor.elm
+++ src/Update/Sponsor.elm
@@ -1,11 +1,15 @@
 module Update.Sponsor exposing (set)
 
+import Dict
 import Model.Model exposing (Model, Msg)
 import Model.Sponsors exposing (Sponsor)
 
 
 set : Model -> Maybe Sponsor -> ( Model, Cmd Msg )
 set model s =
-    ( { model | sponsor = s }
+    ( { model
+        | sponsor = s
+        , vehicles = Dict.map (\k v -> { v | perks = [] }) model.vehicles
+      }
     , Cmd.none
     )
